cee910cfde954d06186bd4a4f9b2e92401f5e8c9
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-06-06 00:26:17 +0200

    Improve example

--- example/app.tsx
+++ example/app.tsx
@@ -1,34 +1,83 @@
 import * as React from 'react';
-import {Component} from 'react';
-import ScrollShadow from '../src';
-import {Item, AppWrapper, AppHeader, AppFooter} from './styled';
+import {Component, ReactNode, ChangeEvent} from 'react';
+import ScrollShadow, { ShaddowColors } from '../src';
+import {ScrollWrapper, Item, AppWrapper, AppHeader, AppFooter, ColorWrapper, ColorsWrapper} from './styled';
 
-export default class App extends Component {
+interface AppProps {
+
+}
+
+interface ShadowPosition {
+  bottomShadowColors: ShaddowColors;
+  topShadowColors: ShaddowColors;
+}
+
+type AppState = ShadowPosition & {
+  shadowSize: number;
+}
+
+const items: ReactNode[] = [];
+
+for (let i = 0; i < 10; i++) {
+  items.push(<Item key={i}>{i}</Item>);
+}
+
+
+export default class App extends Component<AppProps, AppState> {
+  state: AppState = {
+    bottomShadowColors: {
+      active: '#cccccc',
+      inactive: '#ffffff'
+    },
+    topShadowColors: {
+      active: '#cccccc',
+      inactive: '#ffffff'
+    },
+    shadowSize: 2
+  };
+
+  onColorChange = (position: keyof ShadowPosition, property: keyof ShaddowColors) => (e: ChangeEvent<HTMLInputElement>) => {
+    const color = e.target.value;
+    const shadowColors: ShaddowColors = {
+      ...this.state[position],
+      [property]: color
+    };
+
+    this.setState({
+      [position as any]: shadowColors
+    });
+  }
+
   render() {
-    const items = [];
-    for (let i = 0; i < 10; i++) {
-      items.push(<Item key={i}>{i}</Item>);
-    }
+    const {bottomShadowColors, topShadowColors, shadowSize} = this.state;
 
     return (
       <AppWrapper>
-        <AppHeader>Header</AppHeader>
-        <ScrollShadow
-          bottomShadowColors={{
-            active: 'gray',
-            inactive: 'white'
-          }}
-
-          topShadowColors={{
-            active: 'gray',
-            inactive: 'white'
-          }}
-
-          shadowSize={2}
-        >
-          {items}
-        </ScrollShadow>
-        <AppFooter>Footer</AppFooter>
+        <ColorsWrapper>
+          <ColorWrapper>
+            Top shadow active: <input type="color" value={topShadowColors.active} onChange={this.onColorChange('topShadowColors', 'active')} />
+          </ColorWrapper>
+          <ColorWrapper>
+            Top shadow inactive: <input type="color" value={topShadowColors.inactive} onChange={this.onColorChange('topShadowColors', 'inactive')} />
+          </ColorWrapper>
+          <ColorWrapper>
+            Bottom shadow active: <input type="color" value={bottomShadowColors.active} onChange={this.onColorChange('bottomShadowColors', 'active')} />
+          </ColorWrapper>
+          <ColorWrapper>
+            Bottom shadow inactive: <input type="color" value={bottomShadowColors.inactive} onChange={this.onColorChange('bottomShadowColors', 'inactive')} />
+          </ColorWrapper>
+        </ColorsWrapper>
+        <ScrollWrapper>
+          <AppHeader>Header</AppHeader>
+          <ScrollShadow
+            topShadowColors={topShadowColors}
+            bottomShadowColors={bottomShadowColors}
+            shadowSize={shadowSize}
+          >
+            {items}
+          </ScrollShadow>
+          <AppFooter>Footer</AppFooter>
+        </ScrollWrapper>
       </AppWrapper>
     );
   }
--- example/styled.ts
+++ example/styled.ts
@@ -8,6 +8,10 @@ injectGlobal`
     font-size: 14px;
   }
 
+  body {
+    background: #69b7eb;
+  }
+
   #app {
     height: 100%;
   }
@@ -20,20 +24,53 @@ export const Item = styled.div`
 
 export const AppWrapper = styled.div`
   height: 100%;
-  width: 400px;
+  width: 800px;
   margin: 0 auto;
   display: flex;
-  flex-direction: column;
+  max-height: 80vh;
+  max-width: 90vw;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 3px;
+  overflow: hidden;
+  background-color: white;
+  box-shadow: 1px 5px 5px 1px rgba(0,0,0,.3);
 `;
 
 export const AppHeader = styled.div`
   flex-shrink: 0;
   height: 40px;
   width: 100%;
+  text-align: center;
 `;
 
 export const AppFooter = styled.div`
   flex-shrink: 0;
   height: 40px;
   width: 100%;
+  text-align: center;
+`;
+
+export const ColorWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 50px;
+  border-bottom: 1px solid #ccc;
+  align-items: center;
+`;
+
+export const ColorsWrapper = styled.div`
+  overflow: auto;
+  padding: 20px;
+`;
+
+export const ScrollWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  margin-left: 30px;
 `;
